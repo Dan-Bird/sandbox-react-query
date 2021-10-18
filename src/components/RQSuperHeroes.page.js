96e76f62047b3c84403ffb00d743b64b8e28f2f7
Daniel <info@daniel-bird.co.uk>
2021-10-18 10:30:23 +0100

    Handle errors

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -4,10 +4,17 @@ import axios from 'axios';
 const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    'super-heroes',
+    fetchSuperHeroes
+  );
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
 
   return (
