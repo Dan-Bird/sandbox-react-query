e745fe1bd77e2afd87bb146df81a6922b2d9bc1f
Daniel <info@daniel-bird.co.uk>
2021-10-18 10:50:34 +0100

    Fetch data on click of button instead of onMount

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -4,18 +4,19 @@ import axios from 'axios';
 const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery(
+  const { isLoading, isFetching, data, isError, error, refetch } = useQuery(
     'super-heroes',
     fetchSuperHeroes,
     {
+      enabled: false,
       // cacheTime: 5000,
       // staleTime: 30_000,
-      refetchInterval: 2000,
-      refetchIntervalInBackground: true,
+      // refetchInterval: 2000,
+      // refetchIntervalInBackground: true,
     }
   );
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
 
@@ -26,6 +27,7 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map(hero => {
         return <div key={hero.name}>{hero.name}</div>;
       })}
